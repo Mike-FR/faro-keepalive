d3953c44198e4ec3ba7d71f4d8d23473de8ea5b6
Mickael Spennato <mickael.spennato@socotec.com>
2023-06-01 16:24:20 +0200

    setup for test faro keepalive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button type="button" @click="logError">Log Error</button> 
+    <br/>
+    <button type="button" @click="raiseException">Raise Exeception</button>
   </div>
 </template>
 
 <script>
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      nbError: 2,
+      sentenceCharSize: 35000
+    }
+  },
+  methods: {
+    generateString(length) {
+        let result = ' ';
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return result;
+    },
+    logError() {
+      // eslint-disable-next-line no-unused-vars
+      for (const i of Array.from(Array(this.nbError).keys())
+      ) {
+        console.error(this.generateString(this.sentenceCharSize))
+      }
+    },
+    raiseException() {
+      for (const i of Array.from(Array(this.nbError).keys())
+      ) {
+        try {
+          // eslint-disable-next-line no-undef
+          methodNotDefined(i)
+        } catch (error) {
+          console.error(i +" methodNotDefined catch")
+        }
+      
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+button  {
+  margin: 1rem;
+  cursor: pointer;
+  outline: 0;
   display: inline-block;
-  margin: 0 10px;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 1rem;
+  border-radius: .25rem;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  color: #fd0d0d;
+  border-color: #fd0d0d;                
 }
-a {
-  color: #42b983;
-}
+
+button:hover {
+  color: #fff;
+  background-color: #fd0d0d;
+}    
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import SocioManifest from "../socio-manifest.json";
+import PackageJson from "../package.json";
+import {initializeTracingAndLogging} from "@socotec.io/socio-vue-components";
+
+initializeTracingAndLogging(SocioManifest, PackageJson, { useDocumentLoadInstrumentation: true, maxExportBatchSize: 20});
+
 
 Vue.config.productionTip = false
 
